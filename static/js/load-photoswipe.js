ca0e91612f5b3929038814879d81319839eb4d8a
Javi Pulido <pulimento@gmail.com>
2020-05-18 20:09:30 +0200

    Update PhotoSwipe library, remove jQuery dependency, customize loading script to get dimensions from markup

--- static/js/load-photoswipe.js
+++ static/js/load-photoswipe.js
@@ -1,71 +1,61 @@
-/*
-Put this file in /static/js/load-photoswipe.js
-Documentation and licence at https://github.com/liwenyip/hugo-easy-gallery/
-*/
-
-/* Show an alert if this js file has been loaded twice */
 if (window.loadphotoswipejs) {
-	window.alert("You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6")
+	console.log("You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6")
 } 
 var loadphotoswipejs = 1;
 
+var verboseLogging = true;
+
 /* TODO: Make the share function work */
-/* TODO remove jQuery dependency (remove it from photos/list.html) */
 /* TODO differentiate between thumbnails and full image */
 
-/*
-JQUERY REMOVAL WIP
 if (document.readyState != 'loading') {
 	OnDocumentReady();
 } else {
 	document.addEventListener('DOMContentLoaded', OnDocumentReady);
 }
 
-
 function OnDocumentReady() {
-*/
 
-
-$( document ).ready(function() {
-	/*
-	Initialise Photoswipe
-	*/
 	var items = []; // array of slide objects that will be passed to PhotoSwipe()
-	// for every figure element on the page:
-	$('figure').each( function() {
-		if ($(this).attr('class') == 'no-photoswipe') return true; // ignore any figures where class="no-photoswipe"
+
+	var figures = document.querySelectorAll("figure");        
+	Array.prototype.forEach.call(figures, function (figure, i) { 
+
+		if(figure.classList.contains('no-photoswipe')) return true; // ignore any figures where class="no-photoswipe"
+
 		// get properties from child a/img/figcaption elements,
-		var $figure = $(this),
-			$a 		= $figure.find('a'),
-			$img 	= $figure.find('img'),
-			$src	= $a.attr('href'),
-			$title  = $img.attr('alt'),
-			$msrc	= $img.attr('src');
-		// if data-size on <a> tag is set, read it and create an item
-		if ($a.data('size')) {
-			var $size 	= $a.data('size').split('x');
+		var a 		= figure.querySelector("a"),
+			img 	= figure.querySelector("img"),
+			src   	= a.getAttribute("href"),
+			title   = img.getAttribute("alt"),
+			msrc	= img.getAttribute("src");
+		
+		if (img.getAttribute('imgH')) {
+			var height 	= img.getAttribute('imgH');
+			var width 	= img.getAttribute('imgW');
 			var item = {
-				src		: $src,
-				w		: $size[0],
-				h 		: $size[1],
-				title 	: $title,
-				msrc	: $msrc
+				src		: src,
+				w		: width,
+				h 		: height,
+				title 	: title,
+				msrc	: msrc
 			};
-			console.log("Using pre-defined dimensions for " + $src);
-		// if not, set temp default size then load the image to check actual size
+			log("Using pre-defined dimensions for " + src + "(" + width + "x" + height + ")");
+		
 		} else {
+			// if not, set temp default size then load the image to check actual size
 			var item = {
-				src		: $src,
+				src		: src,
 				w		: 800, // temp default size
 				h 		: 600, // temp default size
-				title 	: $title,
-				msrc	: $msrc
+				title 	: title,
+				msrc	: msrc
 			};
-			console.log("Using default dimensions for " + $src);
+			log("Using default dimensions for " + src);
 			// load the image to check its dimensions
 			// update the item as soon as w and h are known (check every 30ms)
 			var img = new Image(); 
-			img.src = $src;
+			img.src = src;
 			var wait = setInterval(function() {
 				var w = img.naturalWidth,
 					h = img.naturalHeight;
@@ -73,24 +63,32 @@ $( document ).ready(function() {
 					clearInterval(wait);
 					item.w = w;
 					item.h = h;
-					console.log("Got actual dimensions for " + img.src);
+					log("Got actual dimensions for " + img.src);
 				}
 			}, 30);
-	   	}
+			}
+			
 		// Save the index of this image then add it to the array
 		var index = items.length;
 		items.push(item);
+
 		// Event handler for click on a figure
-		$figure.on('click', function(event) {
+		figure.addEventListener('click', function(event) {
 			event.preventDefault(); // prevent the normal behaviour i.e. load the <a> hyperlink
 			// Get the PSWP element and initialise it with the desired options
-			var $pswp = $('.pswp')[0];
+			var pswp = document.querySelector('.pswp');
 			var options = {
 				index: index, 
 				bgOpacity: 0.8,
 				showHideOpacity: true
 			}
-			new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options).init();
+			new PhotoSwipe(pswp, PhotoSwipeUI_Default, items, options).init();
 		});	
 	});
-});
+}
+
+function log(text) {
+	if(verboseLogging) {
+		console.log(text);
+	}
+}
